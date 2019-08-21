a1d19f1ecc76e0f9aa26bb49a2c4b30bca24b21a
Панов Антон <A.Panov@expobank.ru>
2019-08-21 17:02:20 +0300

    update

--- data_collector2.js
+++ data_collector2.js
@@ -1,44 +1,112 @@
 
 const { app, BrowserWindow, dialog } = require('electron')
+const fs = require('fs')
+
+class DOMElement
+{
+    constructor(oWindow_i, )
+    {
+
+    }
+}
 
 class AutoRuParser
 {
     constructor(oWindow_i)
     {
         this.oWindow = oWindow_i
-        this.iWaitTime = 1000;
-        
-        this.fnStart()
-    }
+        this.iWaitTime = 1000
+        this.sSavedURLsFileName = "auto_ru_URLS.old.json"
+        this.aBrands = {}
+        this.oModels = {}
+        this.oURLs = {}
 
-    async fnWaitOneElementCSS(sCSS, iWaitTime = this.iWaitTime)
-    {
-        var oNodeList = await this.fnWaitElementCSS(sCSS, iWaitTime)
-
-        if (oNodeList && oNodeList.length) {
-            return oNodeList[0]
+        try {
+            this.fnLoadURLs()
+            this.fnStart()
+        } catch(oException) {
+            console.log(`[E] ${oException.message}`)
         }
     }
 
-    async fnWaitElementCSS(sCSS, iWaitTime = this.iWaitTime)
+    fnLoadURLs(sFileName = this.sSavedURLsFileName)
     {
+        if (fs.existsSync(sFileName)) {
+            JSON.parse(fs.readFileSync(
+				sFileName,
+				"utf8"
+			))
+        }
+    }
+
+    fnSaveURLs(sFileName = this.sSavedURLsFileName)
+    {
+        fs.writeFile(
+            sFileName, 
+            JSON.stringify(this.oURLs)
+        )
+    }
+
+    async fnGetElementCSS(sCSS, iWaitTime = this.iWaitTime)
+    {
+        console.log(`[!] fnGetElementCSS '${sCSS}' ${iWaitTime}s`);
+
         var iStartTime = Math.round(new Date().getTime()/1000);
 
         while(true) {
             var iEndTime = Math.round(new Date().getTime()/1000);
             
             if (iEndTime-iStartTime>=iWaitTime) {
+                console.log(`[E] fnGetElementCSS '${sCSS}' ${iWaitTime}s - Timeout`);
                 break;
             }
 
-            var oNodeList = await this.oWindow.webContents.executeJavaScript(`document.querySelectorAll('${sCSS}')`)
+            var iResult = await this.oWindow.webContents.executeJavaScript(`
+                (function() { 
+                    var oNodeList = document.querySelectorAll('${sCSS}');
+                    if (oNodeList.length>0) {
+                        if (!window.SAVED_ELEMENTS) {
+                            window.SAVED_ELEMENTS = [];
+                        }
+                        window.SAVED_ELEMENTS.push(oNodeList[0]);
+                        return window.SAVED_ELEMENTS.length-1;
+                    }
+                    return -1;
+                })()
+            `)
 
-            console.log('oNodeList > ', oNodeList);
-
-            if (oNodeList.length) {
-                return oNodeList;    
+            if (iResult!=-1) {
+                console.log(`[E] fnGetElementCSS '${sCSS}' ${iWaitTime}s - Create element with id ${iResult}`);
+                return new DOMElement();    
             }
         }
+
+        console.log(`[E] fnGetElementCSS '${sCSS}' ${iWaitTime}s - Element not found`);
+    }
+
+    async fnWaitElementCSS(sCSS, iWaitTime = this.iWaitTime)
+    {
+        console.log(`[!] fnWaitElementCSS '${sCSS}' ${iWaitTime}s`);
+
+        var iStartTime = Math.round(new Date().getTime()/1000);
+
+        while(true) {
+            var iEndTime = Math.round(new Date().getTime()/1000);
+            
+            if (iEndTime-iStartTime>=iWaitTime) {
+                console.log(`[E] fnWaitElementCSS '${sCSS}' ${iWaitTime}s - Timeout`);
+                break;
+            }
+
+            var iResult = await this.oWindow.webContents.executeJavaScript(`document.querySelectorAll('${sCSS}').length`)
+
+            if (iResult) {
+                console.log(`[E] fnWaitElementCSS '${sCSS}' ${iWaitTime}s - Found ${iResult} elements`);
+                return true;    
+            }
+        }
+
+        console.log(`[E] fnWaitElementCSS '${sCSS}' ${iWaitTime}s - Element not found`);
     }
 
     async fnStart()
@@ -46,11 +114,12 @@ class AutoRuParser
         console.log('START PARSING');
 
         await this.fnLoadURL('https://auto.ru/moskva/cars/all/')
-        //dialog.showMessageBoxSync({message:'123'})
 
-        var oSelectButton = await this.fnWaitOneElementCSS(".Select__button")
+        if (!await this.fnWaitElementCSS(".Select__button")) {
+            new Error("10001 - Button not found");
+        }
 
-        console.log('oSelectButton > ', oSelectButton)
+        console.log('bSelectButtonExists > ', bSelectButtonExists)
     }
 
     async fnLoadURL(sURL)
