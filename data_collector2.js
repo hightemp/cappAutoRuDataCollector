f40b9dd3835a59470b35981474c675bf819581a9
Панов Антон <A.Panov@expobank.ru>
2019-08-22 18:04:28 +0300

    update

--- data_collector2.js
+++ data_collector2.js
@@ -63,7 +63,9 @@ class DOMElement
         var fX = (oBoundingClientRect.right + oBoundingClientRect.left)/2;
         var fY = (oBoundingClientRect.bottom + oBoundingClientRect.top)/2;
 
-        await this.fnSendInputClickEvent({ x:fX, y:fY })
+        for (let iIndex = 0; iIndex < iClickCount; iIndex++) {
+            await this.fnSendInputClickEvent({ x:fX, y:fY })
+        }
     }
 }
 
@@ -74,7 +76,7 @@ class AutoRuParser
         this.oWindow = oWindow_i
         this.iWaitTime = 1000
         this.sSavedURLsFileName = "auto_ru_URLS.old.json"
-        this.aBrands = {}
+        this.aBrands = []
         this.oModels = {}
         this.oURLs = {}
 
@@ -105,7 +107,110 @@ class AutoRuParser
         )
     }
 
-    async fnGetElementCSS(sCSS, iWaitTime = this.iWaitTime)
+    async fnGetElementsAttributeXPath(sXPath, aAttributeName, iWaitTime = this.iWaitTime)
+    {
+        console.log(`[!] fnGetElementsAttributeXPath '${sXPath}' ${iWaitTime}s`);
+
+        var iStartTime = Math.round(new Date().getTime()/1000);
+
+        while(true) {
+            var iEndTime = Math.round(new Date().getTime()/1000);
+            
+            if (iEndTime-iStartTime>=iWaitTime) {
+                console.log(`[E] fnGetElementsAttributeXPath '${sXPath}' ${iWaitTime}s - Timeout`);
+                break;
+            }
+
+            var aResult = await this.oWindow.webContents.executeJavaScript(`
+                (function() { 
+                    var oXPathResult = document.evaluate('${sXPath}', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+                    if (oXPathResult.snapshotLength>0) {
+                        var aResult = [];
+
+                        for (var iIndex=0; iIndex<oXPathResult.snapshotLength; iIndex++) {
+                            aResult.push(oXPathResult.snapshotItem(iIndex)['${aAttributeName}']);
+                        }
+
+                        return aResult;
+                    }
+                    return false;
+                })()
+            `)
+
+            if (aResult) {
+                console.log(`[+] fnGetElementsAttributeXPath '${sXPath}' ${iWaitTime}s - ${aResult.length}`);
+                return aResult;    
+            }
+        }
+
+        console.log(`[E] fnGetElementsAttributeXPath '${sXPath}' ${iWaitTime}s - Element not found`);
+    }
+
+    async fnGetElementXPath(sXPath, iElementIndex=0, iWaitTime = this.iWaitTime)
+    {
+        console.log(`[!] fnGetElementXPath '${sXPath}' ${iWaitTime}s`);
+
+        var iStartTime = Math.round(new Date().getTime()/1000);
+
+        while(true) {
+            var iEndTime = Math.round(new Date().getTime()/1000);
+            
+            if (iEndTime-iStartTime>=iWaitTime) {
+                console.log(`[E] fnGetElementXPath '${sXPath}' ${iWaitTime}s - Timeout`);
+                break;
+            }
+
+            var iResult = await this.oWindow.webContents.executeJavaScript(`
+                (function() { 
+                    var oXPathResult = document.evaluate('${sXPath}', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+                    if (oXPathResult.snapshotLength>0) {
+                        if (!window.SAVED_ELEMENTS) {
+                            window.SAVED_ELEMENTS = [];
+                        }
+                        window.SAVED_ELEMENTS.push(oXPathResult.snapshotItem(${iElementIndex}));
+                        return window.SAVED_ELEMENTS.length-1;
+                    }
+                    return -1;
+                })()
+            `)
+
+            if (iResult!=-1) {
+                console.log(`[+] fnGetElementXPath '${sXPath}' ${iWaitTime}s - Create element with id ${iResult}`);
+                return new DOMElement(this.oWindow, iResult);    
+            }
+        }
+
+        console.log(`[E] fnGetElementXPath '${sXPath}' ${iWaitTime}s - Element not found`);
+    }
+
+    async fnWaitElementXPath(sXPath, iWaitTime = this.iWaitTime)
+    {
+        console.log(`[!] fnWaitElementXPath '${sXPath}' ${iWaitTime}s`);
+
+        var iStartTime = Math.round(new Date().getTime()/1000);
+
+        while(true) {
+            var iEndTime = Math.round(new Date().getTime()/1000);
+            
+            if (iEndTime-iStartTime>=iWaitTime) {
+                console.log(`[E] fnWaitElementXPath '${sXPath}' ${iWaitTime}s - Timeout`);
+                break;
+            }
+
+            var iResult = await this.oWindow.webContents.executeJavaScript(`
+                document.evaluate('${sXPath}', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength
+            `)
+
+            if (iResult) {
+                console.log(`[+] fnWaitElementXPath '${sXPath}' ${iWaitTime}s - Found ${iResult} elements`);
+                return true;    
+            }
+        }
+
+        console.log(`[E] fnWaitElementXPath '${sXPath}' ${iWaitTime}s - Element not found`);
+    }
+
+    async fnGetElementCSS(sCSS, iElementIndex=0, iWaitTime = this.iWaitTime)
     {
         console.log(`[!] fnGetElementCSS '${sCSS}' ${iWaitTime}s`);
 
@@ -126,7 +231,7 @@ class AutoRuParser
                         if (!window.SAVED_ELEMENTS) {
                             window.SAVED_ELEMENTS = [];
                         }
-                        window.SAVED_ELEMENTS.push(oNodeList[0]);
+                        window.SAVED_ELEMENTS.push(oNodeList[${iElementIndex}]);
                         return window.SAVED_ELEMENTS.length-1;
                     }
                     return -1;
@@ -177,11 +282,46 @@ class AutoRuParser
             new Error("10001 - Button not found");
         }
 
-        var oElement;
+        var oBodyElement = await this.fnGetElementCSS("body")
 
-        if (oElement = await this.fnGetElementCSS(".Select__button")) {
-            await oElement.fnClick();
+        // Открытие списка с марками(брендами)
+        var oElement = await this.fnGetElementCSS(".Select__button")
+
+        if (!oElement) {
+            console.log(`[E] Parsing brands list - Element not found '.Select__button'`)
+            return;
         }
+
+        await oElement.fnClick()
+
+        await oBodyElement.fnClick()
+
+        // Получение списка с марками(брендами)
+        var sBrandMenuItemXPath = "(//*[contains(@class,\"Select__menu\")])//*[contains(@class,\"Menu__group\")][descendant::*[text()=\"Все\"]]/*[contains(@class,\"MenuItem\")]"
+        this.aBrands = await this.fnGetElementsAttributeXPath(sBrandMenuItemXPath, 'innerText')
+
+        for (var sBrand of this.aBrands) {
+            oURLS[sBrand] = {}
+
+            var oElement = await this.fnGetElementCSS(".Select__button")
+
+            if (!oElement) {
+                console.log(`[E] Iterating brands list - '${sBrand}' - Element not found '.Select__button'`)
+                return;
+            }
+    
+            await oElement.fnClick()
+    
+            var sBrandMenuItemXPath = "(//*[contains(@class,\"Select__menu\")])[1]//div[text()=\"Все\"]/following::*[text()=\""+sBrand+"\"]"
+            
+        }
+
+        console.dir(aBrands, {depth: null, colors: true, maxArrayLength: null})
+    }
+
+    fnConsoleDir(aValue)
+    {
+        console.dir(aValue, {depth: null, colors: true, maxArrayLength: null})
     }
 
     async fnLoadURL(sURL)
